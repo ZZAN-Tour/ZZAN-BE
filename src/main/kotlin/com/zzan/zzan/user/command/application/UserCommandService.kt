acc28e063040e73bbdd53750dc8a9be10a09eae1
선빈 <ehtjsqls@gmail.com>
2025-09-06 04:06:16 +0900

    feat: 사용자 스크랩 기능 추가 및 관련 API 엔드포인트 구현

--- src/main/kotlin/com/zzan/zzan/user/command/application/UserCommandService.kt
+++ src/main/kotlin/com/zzan/zzan/user/command/application/UserCommandService.kt
@@ -1,7 +1,16 @@
 package com.zzan.zzan.user.command.application
 
+import com.zzan.zzan.api.user.dto.ScrapResponse
 import com.zzan.zzan.user.command.domain.User
 
 interface UserCommandService {
-    fun createUser(user: User): String
+    fun createUser(user: User): User
+
+    fun createFeedScrap(userId: String, feedId: String): ScrapResponse
+
+    fun createLiquorScrap(userId: String, liquorId: String): ScrapResponse
+
+    fun deleteFeedScrap(userId: String, feedId: String): ScrapResponse
+
+    fun deleteLiquorScrap(userId: String, liquorId: String): ScrapResponse
 }
